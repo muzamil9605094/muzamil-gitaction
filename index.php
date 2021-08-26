93abe678af7f01376048ec21ea3c09b9b2686fb0
sajjadtalib29 <sajjadtalib29@gmail.com>
2021-08-26 14:59:29 +0500

    commit files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>GitHub Action</title>
+</head>
+<?php
+echo "This is PHP Code";
+?>
+<body>
+<h1>My name is Sajjad Talib</h1>
+<p>This is CI CD Pipeline</p>
+</body>
+</html>
